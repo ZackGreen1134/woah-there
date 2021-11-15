496f7596cbb8bfc1126c4636afac4f243a68687a
ZackGreen1134 <76422009+ZackGreen1134@users.noreply.github.com>
2021-11-15 12:59:21 -0500

    d

---
+++ nulleds_big_ass_forehead.lua
@@ -0,0 +1,17 @@
+--[[
+    NOT STEAM ID'S
+]]
+
+
+--[[
+    format for yui
+    76561199055269582, 
+    AND THEY'RE WHITELISETD!! OMG OMG OMG
+
+    takes dick years to update from github to this
+]]
+
+76561199055269582, -- yui
+76561198358598390, -- yui 2
+76561198322873144, -- shit finger
+76561198982197387, -- zack :weary:
